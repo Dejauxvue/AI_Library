34b80ead89fac77b848644fa2f07f4456f29511e
Dejauxvue <josuabloess@gmx.de>
2015-08-22 18:18:54 +0200

    changed build to pure ai trial, still fails

--- AI_Library/main.cpp
+++ AI_Library/main.cpp
@@ -1,25 +1,29 @@
-//
-//#include <iostream>
-//
-//
-//#include "Jubics_crube_problem.h"
-//#include "AI_search_tree.h"
-//
-//
-//int main(int argc, char* argv){
-//	std::cout << "Hello Mars" << std::endl;
-//
-//	jubics_crube::Problem_impl problem;
-//
-//	AI_search_tree tree;
-//
-//	tree.set_input(problem);
-//
-//	std::list<std::shared_ptr<Action>> result;
-//
-//	std::cout << "depth search result: " << tree.depth_search(result)<< std::endl;
-//
-//
-//	getchar();
-//	return 0;
-//}
+
+#include <iostream>
+
+
+#include "Jubics_crube_problem.h"
+#include "AI_search_tree.h"
+
+
+int main(int argc, char* argv){
+	std::cout << "Hello Mars" << std::endl;
+
+	jubics_crube::Problem_impl problem;
+
+	AI_search_tree tree;
+
+	tree.set_input(problem);
+
+	std::list<std::shared_ptr<Action>> result;
+
+	std::cout << "depth search result: " << tree.depth_search(result)<< std::endl;
+
+	for (std::list<std::shared_ptr<Action>>::iterator iter = result.begin(); iter != result.end(); iter++){
+		
+	}
+
+
+	getchar();
+	return 0;
+}
